befdfdf284810a1123bab81dd13c4e7c9038edba
AnkitEsmagico <ankit.k@esmagico.in>
2025-09-19 19:45:20 +0530

    icon fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,14 +34,16 @@ export const metadata = {
     maximumScale: 1,
     userScalable: false,
   },
-
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Upskillr" />
