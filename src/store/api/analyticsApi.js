4c629b35e396491dc2f88b47af2397e6a058a16d
AnkitEsmagico <ankit.k@esmagico.in>
2025-11-14 15:51:01 +0530

    user analytics API integration done

--- src/store/api/analyticsApi.js
+++ src/store/api/analyticsApi.js
@@ -1,33 +1,23 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const dummyResponse = {
-  summary_metrics: [
-    {
-      title: "# Total Presentations",
-      value: "1"
-    },
-    {
-      title: "# Total Active Users", 
-      value: "6"
-    },
-    {
-      title: "Average Completion Rate",
-      value: "0.00%"
-    }
-  ]
-};
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithReauthAndRetry } from "./baseQuery";
 
 export const analyticsApi = createApi({
   reducerPath: "analyticsApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "/api/analytics/",
-  }),
+  baseQuery: baseQueryWithReauthAndRetry,
   endpoints: (builder) => ({
     getPresentationStats: builder.query({
-      queryFn: () => ({ data: dummyResponse }),
+      query: () => ({
+        url: "api/users/analytics/presentations",
+        method: "POST",
+        body: {},
+      }),
     }),
     getAssessmentStats: builder.query({
-      queryFn: () => ({ data: dummyResponse }),
+      query: () => ({
+        url: "api/users/analytics/assessments",
+        method: "POST",
+        body: {},
+      }),
     }),
   }),
 });
